ba382ebaaa2dd94f031d431b257bb396caa1b3d6
tom.vanroy <tom.vanroy@ONTCUR-99D73EBD>
2017-05-04 09:54:46 +0200

    Add project files.

---
+++ TestDrivenDevelopment/Persoon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrivenDevelopment
+{
+    public class Persoon
+    {
+        // Constructor
+        public Persoon(List<string> voornaam)
+        {
+            this.voornaam = voornaam;
+        }
+
+        // Property
+        public List<string> voornaam { get; set; }
+
+        // Method
+        public override string ToString()
+        {
+            string result = "";
+            foreach (var eenVoornaam in voornaam)
+            {
+                result += eenVoornaam + " ";
+            }
+            result.Remove(result.Length - 1);
+            return result;
+        }
+    }
+}
---
+++ TestDrivenDevelopmentTest/PersoonTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDevelopment;
+using System.Text.RegularExpressions;
+
+namespace TestDrivenDevelopmentTest
+{
+    [TestClass]
+    public class PersoonTest
+    {
+        private Persoon persoon;
+        [TestInitialize]
+        public void Initialize()
+        {
+            persoon = new Persoon(new System.Collections.Generic.List<string> { "Gert", "Pieter", "Jan" });
+        }
+        [TestMethod]
+        public void PersoonHeeftMinstensEenVoornaam()
+        {
+            Assert.IsTrue(persoon.voornaam.Count > 0 && persoon.voornaam != null);
+        }
+
+        [TestMethod]
+        public void PersoonHeeftNietTweeKeerDezelfdeVoornaam()
+        {
+            foreach (var eenVoornaam in persoon.voornaam)
+            {
+                int amount = 0;
+                foreach (var duplicaatVoornaam in persoon.voornaam)
+                {
+                    if (eenVoornaam == duplicaatVoornaam)
+                    {
+                        amount++;
+                    }
+                }
+                Assert.IsTrue(amount == 1);
+            }
+        }
+
+        [TestMethod]
+        public void ElkeVoornaamHeeftMinstensEenTeken()
+        {
+            Regex reg = new Regex("\\w+?");
+            foreach (var eenVoornaam in persoon.voornaam)
+            {
+                Assert.IsTrue(reg.IsMatch(eenVoornaam));
+            }
+        }
+
+        [TestMethod]
+        public void ToStringGeeftAlleVoornamenGescheidenMetSpaties()
+        {
+            string result = "";
+            foreach (var eenVoornaam in persoon.voornaam)
+            {
+                result += eenVoornaam + " ";
+            }
+            result.Remove(result.LastIndexOf(" "));
+            Assert.AreEqual(persoon.ToString(), result);
+        }
+    }
+}
